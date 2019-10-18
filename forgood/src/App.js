5dbeefc5ff0e1cc32ee35ae6f4edab9cc5c1cca8
schneiderryan <schneiderryan2017@gmail.com>
2019-10-18 19:41:53 -0400

    navbar & initial react project

---
+++ forgood/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Navbar, Nav, Form, FormControl} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light" sticky="top">
+      <Navbar.Brand href="#home">Apprisen</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Logo</Nav.Link>
+        <Nav.Link href="#features">Resources</Nav.Link>
+        <Nav.Link href="#pricing">Forum</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+      <header className="App-header">
+  <br />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the homepage
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
