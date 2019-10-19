158bfc995c8d677e70111b17469492c4c9de3240
Bryan Lliguicota <Bryan.Lliguicota97@gmail.com>
2019-10-19 10:41:32 -0400

    More Diagramsgit add .

--- src/Stats.js
+++ src/Stats.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Statistic, Card, Row, Col, Icon,Progress } from 'antd';
+import UserComments from './UserComments'
 
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+});
+const marginIcon = {margin:'5px'}
+const marginProgress = {margin:'20px'}
+const rowMargin= {margin:'20px'}
+const marginLeft = {margin:'20px'}
 class Stats extends React.Component{
 
     render(){
@@ -29,13 +37,14 @@ class Stats extends React.Component{
                        '100%': '#87d068',
                      }}
                      percent={29.6}
-                   />
+                   style={marginProgress}/>
                    <Progress
                      strokeColor={{
                        from: '#108ee9',
                        to: '#87d068',
-                     }}
+                    }}
                      percent={54.9}
+                     style={marginProgress}
                      status="active"
                    />
                    <Progress
@@ -45,6 +54,7 @@ class Stats extends React.Component{
                        '100%': '#87d068',
                      }}
                      percent={50}
+                     style={marginProgress}
                    />
                    <Progress
                      type="circle"
@@ -53,9 +63,10 @@ class Stats extends React.Component{
                        '100%': '#87d068',
                      }}
                      percent={14}
+                     style={marginProgress}
                    />
               </div>
-               <div class="col-sm">
+               <div class="col-sm" style={marginLeft}>
                  <Row gutter={16}>
                  <Col span={12}>
                    <Statistic title="Next Payment Date" value={"October 25, 2019"} />
@@ -69,25 +80,25 @@ class Stats extends React.Component{
              </div>
 
 
-             <div class="row">
+             <div class="row" style={rowMargin}>
 
                <div class="col-sm">
                <Row gutter={16}>
                  <Col span={12}>
-                   <Statistic title="Feedback" value={1128} prefix={<Icon type="like" />} />
+                   <Statistic title="Feedback" value={11128} prefix={<Icon type="like" />} />
                  </Col>
                  <Col span={12}>
-                   <Statistic title="Unmerged" value={93} suffix="/ 100" />
+                    <div className="icons-list">
+                    <IconFont type="icon-tuichu" style={marginIcon} />
+                    <IconFont type="icon-facebook" style={marginIcon}/>
+                    <IconFont type="icon-twitter" style={marginIcon}/>
+                    </div>
                  </Col>
                </Row>
                </div>
 
-               <div class="col-sm">
-                TMP
-              </div>
-
-               <div class="col-sm">
-                TMP
+               <div class="col">
+                <UserComments />
                </div>
 
              </div>
