23e1757af59143e80b07d327b280eac99e0e5732
Katherine Legaspi <klega002@ucr.edu>
2019-10-19 11:05:58 -0400

    button func

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -5,24 +5,7 @@ import { Popover, Button } from 'antd';
 import { Form, Input, Radio } from 'antd';
 
 const text = <span>Enter Payment: </span>;
-
-const content = (
-  <div>
-    <Form.Item label="Amount" >
-      <Input placeholder="Enter amount" />
-    </Form.Item>
-    <Form.Item label="Date" >
-      <Input placeholder="Enter date" />
-    </Form.Item>
-    <Form.Item label="Recommended" >
-    <span className="ant-form-text">$20</span>
-    </Form.Item>
-    <Form.Item >
-      <Button type="primary">Submit</Button>
-    </Form.Item>
-</div>
-);
-
+const positionBut = {position:'relative', right:'50px'}
 
 const buttonWidth = 100;
 
@@ -52,8 +35,6 @@ class ProgressBar extends React.Component {
             <div className="bar" style={ progress }><span>{ this.state.progress + "%" }</span></div>
           </div>
 
-            {/* <button onClick={ this.clickHandler.bind(this) }>Make Payment</button> */}
-
             <div className="demo">
             <div
               style={{ marginLeft: buttonWidth, clear: "both", whiteSpace: "nowrap" }}
@@ -61,10 +42,25 @@ class ProgressBar extends React.Component {
               <Popover
                 placement="bottom"
                 title={text}
-                content={content}
+                content={
+                  <div>
+                  <Form.Item label="Amount" >
+                    <Input placeholder="Enter amount" />
+                  </Form.Item>
+                  <Form.Item label="Date" >
+                    <Input placeholder="Enter date" />
+                  </Form.Item>
+                  <Form.Item label="Recommended" >
+                  <span className="ant-form-text">$20</span>
+                  </Form.Item>
+                  <Form.Item >
+                  <button onClick={ this.clickHandler.bind(this) }>Pay</button>
+                  </Form.Item>
+              </div>
+                }
                 trigger="click"
               >
-                <Button>Pay</Button>
+                <Button style={positionBut}>Pay</Button>
               </Popover>
             </div>
           </div>
